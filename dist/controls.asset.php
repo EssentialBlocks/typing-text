d8cf6efc50879da5d0732ce8c77d13ff52c1d31b
Monir <fencemonir@gmail.com>
2022-08-03 14:56:19 +0600

    update style hanler & controls

--- dist/controls.asset.php
+++ dist/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0517e4b3b4ede080e89c532d2a8d2261');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '2b111d8910eba8e20b263c894e4f1d25');
