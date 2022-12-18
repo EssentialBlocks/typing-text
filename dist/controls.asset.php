2276130d842fa304910a634343a563aa5137cc82
Monir <fencemonir@gmail.com>
2022-12-18 16:12:11 +0600

    update readme & rebuild

--- dist/controls.asset.php
+++ dist/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b854d8e9b4bce7fac7683eb4ae178366');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f2fc4dda4dd1e66bcdc2');
