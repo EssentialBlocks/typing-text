7d8445b57e7abfe0c300b7c7aa9902040c026247
Monir <fencemonir@gmail.com>
2023-03-30 15:32:52 +0600

    update dist files

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '27a4663555e8854de3a5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '27cc61738d2cec407242');
